26d696e6828d38f65a134e787b51ff9abb65f532
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-05-23 14:25:36 -0400

    update stuff

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ fs.readFile('./index.html', 'utf8', (err, html) => {
 	if (err) {
 		throw err;
 	}
-	
+
 	http.createServer((request, response) => {
 		response.writeHeader(200, { "Content-Type": "text/html" });
 		response.write(html);
 		response.end();
-	
+
 	}).listen(8000);
 
 	console.log('listening on port 8000');
-
 });
-
