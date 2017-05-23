973716b21af6c0581de57ae128d86c06399cf3d8
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-05-23 16:00:59 -0400

    update routes

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 	res.sendfile(__dirname + '/public/index.html'); 
 });
 
+app.get('/yo', (req, res) => {
+	res.send('yo');
+});
+
 app.listen(process.env.PORT || 3000, () => {
 	console.log('listening to port 3000');	
 });
